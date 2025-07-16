91d3cf71c96f1c83036e4ad804592b9368cffbf6
Marck <kaciker@gmail.com>
2025-07-16 19:35:05 +0200

    Update session.php
    more info user

--- ajax/session.php
+++ ajax/session.php
@@ -1,12 +1,21 @@
 <?php
-/**  FlowiseChat – Endpoint de sesión (plugins/flowisechat/ajax/session.php) */
+/** FlowiseChat – Endpoint de sesión (plugins/flowisechat/ajax/session.php) */
 include_once '../../../inc/includes.php';
 Session::checkLoginUser();
 
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode([
-    'id'   => $_SESSION['glpiID']         ?? null,
-    'name' => $_SESSION['glpiname']       ?? '',
-    'csrf' => $_SESSION['glpicsrf_token'] ?? null,
-    'sid'  => session_id()
-]);
+
+$user = new User();
+if ($user->getFromDB($_SESSION['glpiID'])) {
+    echo json_encode([
+        'id'        => $_SESSION['glpiID'],
+        'username'  => $user->fields['name'],        // login
+        'firstname' => $user->fields['firstname'],   // nombre
+        'lastname'  => $user->fields['realname'],    // apellido
+        'csrf'      => $_SESSION['glpicsrf_token'] ?? null,
+        'sid'       => session_id()
+    ]);
+} else {
+    http_response_code(403);
+    echo json_encode(['error' => 'Usuario no encontrado']);
+}
